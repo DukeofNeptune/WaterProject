792c3a8375573d0776fbee87710c484f54f00683
DukeofNeptune <dallin.duke4@gmail.com>
2023-02-26 22:35:22 -0700

    Add project files.

---
+++ WaterProject/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterProject.Models;
+
+namespace WaterProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IWaterProjectRepository repo;
+
+        public HomeController (IWaterProjectRepository temp)
+        {
+            repo = temp;
+        }
+        
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+            var blah = repo.Projects
+                .OrderBy(p => p.ProjectName)
+                .Skip(pageNum * pageSize)
+                .Take(pageSize);
+                
+
+
+            return View(blah);
+        }
+
+        
+    }
+}
